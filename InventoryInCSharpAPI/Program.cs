ace5cf3d07e4fb651b29423e6080336c53610b76
Zarzoom <SophiaSeely@outlook.com>
2023-03-11 11:16:06 -0600

    program.cs was modified to accept the UI

--- InventoryInCSharpAPI/Program.cs
+++ InventoryInCSharpAPI/Program.cs
@@ -19,7 +19,17 @@ builder.Services.AddSingleton<ItemRepository>();
 builder.Services.AddSingleton<PantryRepository>();
 builder.Services.AddSingleton<PantryContentsRepository>();
 
-
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+  
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                "http://localhost:8000");
+        });
+});  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,5 +42,5 @@ app.UseSwaggerUI();
 // app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials());
 app.Run();
