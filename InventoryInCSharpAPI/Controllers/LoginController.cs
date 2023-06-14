2a4a4806da570427c2fdc26133a876147c369347
Zarzoom <SophiaSeely@outlook.com>
2023-06-14 16:52:44 -0500

    wip

---
+++ InventoryInCSharpAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventoryInCSharpAPI.Managers;
+using InventoryInCSharpAPI.Models;
+
+namespace InventoryInCSharpAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private readonly LoginManager loginManager;
+    public LoginController()
+    {
+        this.loginManager = loginManager;
+    }
+
+    [HttpPost]
+
+    public User Post([FromBody] User postmanUser)
+    {
+        return loginManager.AddLogin(postmanUser);
+    }
+
+    [HttpGet("search/{userName}")]
+
+    public User Get(User)
+    {
+        
+    }
+    
+}
