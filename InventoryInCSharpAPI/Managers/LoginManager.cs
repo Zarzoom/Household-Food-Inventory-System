107748c04fff0bd01043e538d6f564b066ddbd94
Zarzoom <SophiaSeely@outlook.com>
2023-06-14 16:52:44 -0500

    wip

---
+++ InventoryInCSharpAPI/Managers/LoginManager.cs
@@ -0,0 +1,58 @@
+﻿using InventoryInCSharpAPI.Models;
+using InventoryInCSharpAPI.Repositories;
+namespace InventoryInCSharpAPI.Managers;
+
+public class LoginManager
+{
+    private readonly LoginRepository _LR;
+    public LoginManager(LoginRepository LR)
+    {
+        this._LR = LR;
+    }
+
+    public User AddLogin(User newUser)
+    {
+        Boolean unique = NoDuplicates(newUser);
+        if (unique == true)
+        {
+            var results = _LR.AddLogin(newUser);
+            results.Wait();
+            newUser.password = results.Result;
+            return newUser;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Boolean NoDuplicates(User potentialUser)
+    {
+        var foundUserNames = findUserWithUserName(potentialUser);
+        if (foundUserNames == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public User findUserWithUserName(User userForSearch)
+    {
+        var results = _LR.findUserWithUserName(userForSearch.userName);
+        results.Wait();
+        var foundUser = results.Result;
+        return (foundUser);
+    }
+
+    public User findUserWithPassword(User userForSearch)
+    {
+        var results = _LR.findUserWithPassword(userForSearch.password);
+        results.Wait();
+        var foundUser = results.Result;
+        return (foundUser);
+    }
+    
+}
---
+++ InventoryInCSharpAPI/Repositories/LoginRepository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using InventoryInCSharpAPI.Models;
+using MySqlConnector;
+namespace InventoryInCSharpAPI.Repositories;
+
+public class LoginRepository
+{
+    private ConnectionStringAndOtherSecrets CSOS;
+
+    public LoginRepository(ConnectionStringAndOtherSecrets csos)
+    {
+        this.CSOS = csos;
+    }
+    
+    public async Task<int> AddLogin(User user)
+    {
+        using (var connection = new MySqlConnection(CSOS.connection))
+        {
+            var sql = "INSERT INTO Login (UserName) VALUES (@UserName); SELECT LAST_INSERT_ID()";
+            var createdLogin = await connection.QueryAsync<int>(sql, user);
+            return createdLogin.SingleOrDefault();
+        }
+    }
+
+    public async Task<User> findUserWithUserName(String userName)
+    {
+        using (var connection = new MySqlConnection(CSOS.connection))
+        {
+            var sql = "SELECT UserName FROM Login WHERE LOWER(UserName) = LOWER('%{userName}%')";
+            var foundUser = await connection.QueryAsync<User>(sql);
+            return foundUser.SingleOrDefault();
+        }
+    }
+    
+    public async Task<User> findUserWithPassword(int password)
+    {
+        using (var connection = new MySqlConnection(CSOS.connection))
+        {
+            var sql = "SELECT UserName FROM Login WHERE Password= @Password";
+            var foundUser = await connection.QueryAsync<User>(sql);
+            return foundUser.SingleOrDefault();
+        }
+    }
+}
