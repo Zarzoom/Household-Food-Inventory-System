c4de6d8c024902bc140d2c1022133b18e1dcbd56
Zarzoom <SophiaSeely@outlook.com>
2023-07-31 09:32:50 -0500

    Begining less-loader

---
+++ InventoryInCSharpUI/webpack.config.ts
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.less$/i,
+                use: [
+                    // compiles Less to CSS
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+        ],
+    },
+};
