6d41e7c5c6c888ce88fe76e30856b1b35d18c8ea
Zarzoom <SophiaSeely@outlook.com>
2023-08-03 13:09:19 -0500

    create login button appears on a login page

--- InventoryInCSharpUI/src/index.tsx
+++ InventoryInCSharpUI/src/index.tsx
@@ -1,19 +1,24 @@
 ﻿import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useFetcher } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Item from "./Pages/Item";
+import Login from "./Pages/Login";
 import {Provider} from "react-redux"
 import {store} from "./Stores/Store"
 import Pantry from "./Pages/Pantry"
 import MyPantry from "./Pages/MyPantry"
 import "./ReactSuiteOverride.css"
+import {useEffect} from "react";
+import {useAppSelector} from "./Hooks/hooks";
 
 export default function App() {
+    
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    <Route index element={<MyPantry/>}/>
+                    <Route index element={<Login/>}/>
+                    <Route path = "pantryContents" element={<MyPantry/>}/>
                     <Route path = "item" element={<Item/>} />
                     <Route path = "pantry" element={<Pantry/>} />
                 </Route>
