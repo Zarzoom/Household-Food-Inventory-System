711aef2e21e66dc7fa8bfa231583524fbb7507b4
Sophia Seely <sophiaseely@outlook.com>
2024-03-19 09:07:59 -0500

    wip azure oauth

--- InventoryInCSharpUI/src/index.tsx
+++ InventoryInCSharpUI/src/index.tsx
@@ -8,7 +8,22 @@ import {store} from "./Stores/Store"
 import Pantry from "./Pages/Pantry"
 import MyPantry from "./Pages/MyPantry"
 import "./StyleSheet.less"
+import {PublicClientApplication, EventType} from '@azure/msal-browser';
+import {msalConfig} from './AuthConfig';
 
+
+const msalInstance = new PublicClientApplication(msalConfig);
+if(!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length >0)
+{
+    msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0]);
+}
+
+msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+        const account = event.payload.account;
+        msalInstance.setActiveAccount(account);
+    }
+});
 
 export default function App() {
     
@@ -29,6 +44,6 @@ export default function App() {
 const root = ReactDOM.createRoot(document.getElementById('root')as HTMLElement);
 root.render(
     <Provider store = {store}>
-    <App />
+    <App instance = {msalInstance}/>
     </Provider> 
 );
